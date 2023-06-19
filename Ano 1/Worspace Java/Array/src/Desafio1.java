e8751caa0501f69765e6a83a67853072db3c2e71
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-06-19 19:41:09 -0300

    New: Iniciando array - desafio 1

--- Ano 1/Worspace Java/Array/src/Desafio1.java
+++ Ano 1/Worspace Java/Array/src/Desafio1.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Desafio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Scanner sc = new Scanner(System.in);
+		
+		int [] numeric = new int[5];
+		int soma = 0;
+		int cont = 0;
+		
+		for(int i = 0; i < 4; i++) {
+			System.out.println("Informe o "+(i+1)+"° número: ");
+			numeric[i] = sc.nextInt();
+			soma += numeric[i];
+			cont++;
+		}
+		
+		System.out.println("Média: " + (soma/cont));
+    }
 
 }
