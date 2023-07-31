f3868406dd7392f3beec4e9de32af731c567f9e7
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-07-31 19:13:30 -0300

    new: finalizando desafio 1 de matriz em java!

--- Ano 1/Worspace Java/Matrix/src/Desafio1.java
+++ Ano 1/Worspace Java/Matrix/src/Desafio1.java
@@ -1,9 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Desafio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Scanner sc = new Scanner(System.in);
+		
+		int [][] numeric = new int[4][5];
+		int [] somaArrayLinha = new int [4];
+		int totalMatriz = 0;
+		
+		for(int i = 0; i < 4; i++) {
+			int soma = 0;
+			
+			for(int j = 0; j < 5; j++) {	
+				System.out.println("Inserir números na L="+(i+1) + " e na C="+(j+1)+"");
+				numeric[i][j] = sc.nextInt();
+				soma = soma + numeric[i][j];
+			}
+			
+			somaArrayLinha[i] = soma;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			System.out.println("Array linha "+ (i+1) + " = " + somaArrayLinha[i]);
+		}
+    }
 
 }
