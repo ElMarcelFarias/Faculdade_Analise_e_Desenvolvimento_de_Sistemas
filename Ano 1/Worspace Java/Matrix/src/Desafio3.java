e44257c516abe912884ab085a5b6d90c688091b2
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-08-01 20:48:10 -0300

    Change: Finalizando desafio 3 matriz java

--- Ano 1/Worspace Java/Matrix/src/Desafio3.java
+++ Ano 1/Worspace Java/Matrix/src/Desafio3.java
@@ -6,37 +6,36 @@ public class Desafio3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int [][] matrizNumeros1 = new int[3][2];
-		int [][] matrizNumeros2 = new int [2][3];
-		int cont = 0;
-		String numerosIguais = " /n";
+		int [][] m1 = new int[3][2];
 		
 		
-		for(int x = 0; x < 2; x++) {
-			for(int i = 0; i < 4; i++) {
-				for(int j = 0; j < 4; j++) {	
-					System.out.println("Inserir números na M="+(x+1)+" L="+(i+1) + " e na C="+(j+1)+"");
-					if(x == 1) {
-						matrizNumeros2[i][j] = sc.nextInt();
-					} else {
-						matrizNumeros1[i][j] = sc.nextInt();
-					}
-				}
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 2; j++) {
+				m1[i][j] = sc.nextInt();
 			}
 		}
 		
+		int linhas = m1.length;
+		int colunas = m1[0].length;
 		
-		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 4; j++) {	
-				if(matrizNumeros2[i][j] == matrizNumeros1[i][j]) {
-					numerosIguais = numerosIguais + matrizNumeros2[i][j];
-					cont++;
-				}
+		int[][] m2 = new int[colunas][linhas];
+		
+		for(int i = 0; i < linhas; i++) {
+			for(int j = 0; j < colunas; j++) {
+				m2[j][i] = m1[i][j];
 			}
 		}
 		
-		System.out.println("Quantidade de números iguais: "+ cont);
-		System.out.println("Quantidade de números iguais: "+ numerosIguais);
+		for(int i = 0; i < colunas; i++) {
+			for(int j = 0; j < linhas; j++) {
+				System.out.print(m2[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
 		
 		
 		
