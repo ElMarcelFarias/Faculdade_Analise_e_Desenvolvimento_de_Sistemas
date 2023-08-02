c659d14be65c9452170efaf7aa8aabb47d348878
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-08-02 20:49:38 -0300

    Change: Finalizando desafio 1 methods java

--- Ano 1/Worspace Java/Methods/src/Desafio1.java
+++ Ano 1/Worspace Java/Methods/src/Desafio1.java
@@ -1,9 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Desafio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe sua nota: ");
+		float nota = sc.nextFloat();
+		
+		System.out.println(calculoNota(nota));
+		
+    }
+	
+	public static String calculoNota(float nota) {
+		
+		String resultado = " ";
+		
+		if(nota < 5) {
+			resultado = "REPROVADO!";
+		} else if (nota < 7) {
+			resultado = "Recuperação!";
+		} else {
+			resultado = "APROVADO!";
+		}
+		
+		return resultado;
+		
+		
+    }
 
 }
