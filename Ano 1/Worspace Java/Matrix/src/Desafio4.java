15bdbda78df725ae8cecb6605ce064b2369480ab
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-08-01 21:18:25 -0300

    Change: desafio 3 finalizado e iniciando desafio4!

---
+++ Ano 1/Worspace Java/Matrix/src/Desafio4.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Desafio4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int [][] m1 = new int[4][4];
+		
+		
+		
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				m1[i][j] = sc.nextInt();
+			}
+		}
+		
+		
+		
+		
+		
+    }
+
+}
