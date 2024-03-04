99d2e66f4587709b91bdc2ca65a75f71a5384f65
ElMarcelFarias <marcel.leitefarias@gmail.com>
2024-03-04 19:25:35 -0300

    Feat: Finalizado OT15 - todolist com nodejs

---
+++ Ano2/BackEnd-Node/toDoListNode/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.use( express.json());
+app.use( express.urlencoded({
+    extended: true
+}));
+
+
+let tasks = ['Passear com o dog', 'Ir ao mercado', 'Comprar manteiga'];
+
+app.get('/', (req, res) => {
+    res.render('index',{tasksList: tasks});
+});
+
+app.post('/', (req,res) => {
+    tasks.push(req.body.tasks);
+    res.render('index', {tasksList: tasks});
+})
+
+app.listen(8000, () => {
+    console.log('Servidor rodando em http://localhost:8000');
+})
+
+app.get('/deletar/:id', (req, res) => {
+    tasks = tasks.filter(function(val, index) {
+        if(index != req.params.id) {
+            return val;
+        }
+    })
+    res.render('index', {tasksList: tasks})
+})
+
