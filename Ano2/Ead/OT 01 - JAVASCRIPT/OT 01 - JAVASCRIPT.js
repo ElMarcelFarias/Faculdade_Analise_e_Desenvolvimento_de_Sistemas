c2878290d2c547648adb0c318ecb1bf179106c51
ElMarcelFarias <marcel.leitefarias@gmail.com>
2024-09-26 19:54:43 -0300

    Change: alterando idade

--- Ano2/Ead/OT 01 - JAVASCRIPT/OT 01 - JAVASCRIPT.js
+++ Ano2/Ead/OT 01 - JAVASCRIPT/OT 01 - JAVASCRIPT.js
@@ -1,5 +1,5 @@
 let nome = "Marcel Leite de Farias";
-let idade = 31;
+let idade = 20;
 let comidaFavorita = "Pizza";
 
 console.log(`Meu nome é ${nome}, tenho ${idade} anos e minha comida favorita é ${comidaFavorita}.`);
